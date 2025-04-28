660a23c755ca35f745472513670a3a5626af2e2a
Quentin Hauuy <quentinhauuy@hotmail.com>
2025-04-28 09:34:17 +0200

    blabla

--- repo/ex02/src/main.cpp
+++
@@ -1,7 +0,0 @@
-#include "Array.hpp"
-
-int main()
-{
-	Array<int> a(5);
-	std::cout << a.size << std::endl;
-}
--- test/a.cpp
+++
@@ -1,8 +0,0 @@
-#include <iostream>
-
-int main()
-{
-	int * array = new int[1];
-	array[0] = 42;
-	std::cout << array[0] << std::endl;
-}
